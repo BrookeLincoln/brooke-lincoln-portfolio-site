4e06b8145145072a086e047644aa2058fa3c3b22
BrookeLincoln <brooke.lincoln@outlook.com>
2022-05-10 11:39:21 -0500

    Update script.js and srtyles.css

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+"use strict";
+
+function myFunction() {
+  var x = document.getElementById("topnav");
+  if (x.className === "topnav") {
+    x.className += "responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
 const toggleButton = document.getElementsByClassName('toggle-button')
 const navbarLinks = document.getElementsByClassName('navbar-links')
 
