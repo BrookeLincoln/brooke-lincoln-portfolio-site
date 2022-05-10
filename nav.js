3afb9327238453ef93100edab4b1b5d4a3da7988
BrookeLincoln <brooke.lincoln@outlook.com>
2022-05-10 10:59:39 -0500

    update index.html, nav.js and styles.css
    Trying to make navigation bar

--- nav.js
+++ nav.js
@@ -1,1 +1,9 @@
-
+"use strict";
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "navbar") {
+    x.className += "responsive";
+  } else {
+    x.className = "navbar";
+  }
+}
