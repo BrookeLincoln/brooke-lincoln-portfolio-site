af00f2154dd158a93cf7e7f8380add5a48c8a5c7
BrookeLincoln <brooke.lincoln@outlook.com>
2022-05-11 16:22:17 -0500

    Updated nav class on all pages, trying to get nav bar to work

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 "use strict";
 
 function myFunction() {
-  var x = document.getElementById("topnav");
-  if (x.className === "topnav") {
+  var x = document.getElementById("navbar");
+  if (x.className === "navbar") {
     x.className += "responsive";
   } else {
-    x.className = "topnav";
+    x.className = "navbar";
   }
 }
 
